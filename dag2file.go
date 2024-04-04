7c5bfcc21c3292f41a21df5fa0cf6362b1874182
xymdyq99 <470450032@qq.com>
2024-04-04 18:23:44 +0800

    Update dag2file.go

--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,18 @@
 package merkledag
 
-// Hash to file
-func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+import (
+    "errors"
+)
+
+// Hash2File retrieves a file based on the provided hash and path.
+func Hash2File(store KVStore, hash []byte, path string, hp HashPool) ([]byte, error) {
+    // Use the KVStore to fetch the file content based on the hash
+    fileContent, err := store.Get(hash)
+    if err != nil {
+        return nil, err // Return error if unable to fetch content
+    }
+
+    // TODO: Implement logic to navigate the Merkle DAG based on the path
+    // For now, we simply return the fetched file content
+    return fileContent, nil
 }
